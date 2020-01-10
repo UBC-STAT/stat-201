89974fb8b8f15a324b5553ad1e7099acce7c3dae
Tiffany A. Timbers <tiffany.timbers@stat.ubc.ca>
2020-01-10 10:25:56 -0800

    Create install.R

---
+++ install.R
@@ -0,0 +1,7 @@
+install.packages("readr")
+install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("infer")
+install.packages("repr")
+install.packages("testthat")
+install.packages("gridExtra")
