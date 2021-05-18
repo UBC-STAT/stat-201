a77586d45533fc7aab47768a52fc78af6eca0af8
Rodolfo Lourenzutti <rodolfo.lourenzutti@gmail.com>
2021-05-17 17:00:59 -0700

    Working on the syllabus

--- docs/scripts/controller.js
+++ docs/scripts/controller.js
@@ -1,19 +1,24 @@
-function load_page(name) {
+function load_page(name, section) {
     const request = new XMLHttpRequest();
     request.open('GET', name);
     request.responseType = 'document';
     request.onload = () => {
         const response = request.responseXML;
         document.querySelector('main').innerHTML = response.querySelector('main').innerHTML;
-        console.log(request.response.querySelector('main').innerHTML);
-    }
+        console.log(section);
+        if (section !== ""){
+            document.getElementById(section).scrollIntoView(true);
+        }
+    };
     request.send();
 };
 
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('[data-page]').forEach(link => {
+        const page = link.dataset.page.split("#")[0];
+        const section = link.dataset.page.split("#")[1];
         link.onclick = () => {
-            load_page(link.dataset.page);
+            load_page(page, section);
             return false;
         };
     });
