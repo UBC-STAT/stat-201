1f9e4b6c90de1234e7a46eab5d897f84a19c8991
Rodolfo Lourenzutti <rodolfo.lourenzutti@gmail.com>
2021-05-17 23:02:18 -0700

    Working on syllabus

--- docs/scripts/controller.js
+++ docs/scripts/controller.js
@@ -1,25 +1,26 @@
-function load_page(name, section) {
-    const request = new XMLHttpRequest();
-    request.open('GET', name);
-    request.responseType = 'document';
-    request.onload = () => {
-        const response = request.responseXML;
-        document.querySelector('main').innerHTML = response.querySelector('main').innerHTML;
-        console.log(section);
-        if (section !== ""){
-            document.getElementById(section).scrollIntoView(true);
-        }
-    };
-    request.send();
-};
+function load_page(path) {
+    const page = path.split("#")[0];
+    const section = path.split("#")[1];
+
+    console.log(`path: ${path}`);
+    console.log(`page: ${page}`);
+    console.log(`section: ${section}`);
+
+    fetch(page)
+        .then(response => response.text())
+        .then(text => {
+            document.querySelector('main').innerHTML = text;
+            if (section) {
+                document.getElementById(section).scrollIntoView(true);
+            }
+        });
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('[data-page]').forEach(link => {
-        const page = link.dataset.page.split("#")[0];
-        const section = link.dataset.page.split("#")[1];
-        link.onclick = () => {
-            load_page(page, section);
-            return false;
-        };
+        link.addEventListener("click", () => {
+            console.log(link.dataset.page);
+            load_page(link.dataset.page);
+        });
     });
 });
